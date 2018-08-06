2ba1b9fa1bcadc33d8d67a919f5b8da76cddd502
Leo Metelys <leo.metelys@gmail.com>
2018-08-06 10:23:28 -0400

    Busca Diário Oficial ALE-AM
    Aplicativo em ASP.NET de uso interno com acesso pela Intranet da ALE-AM para a pesquisa de termos no diário oficial eletrônico da ALE-AM

---
+++ BuscarPDF/BuscarPDF/Default.aspx.vb
@@ -0,0 +1,459 @@
+﻿'Aplicativo de Busca de Diários Oficiais
+'Data Criação: 06/08/2015 11:22h
+'Objetivo: Fornecer aos servidores da Casa Legislativa um mecanismo de busca rápido de textos
+'          contidos nos diários oficiais eletrônicos já criados
+'Desenvolvedor: Leonardo Metelys
+'Gerência de Aplicativos - Diretoria de Informática 
+
+
+Imports System.Drawing
+Imports System.Runtime
+Imports System.IO
+
+
+Public Class _Default
+    Inherits Page
+
+    'Texto a ser procurado
+    Public Stexto = ""
+
+    'Endereço de busca dos PDF´s na rede
+    Public sEndereco As String = "\\172.16.0.31\dol"
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+        TxtPesq.Focus()
+
+        Chk2018.Checked = True
+
+    End Sub
+
+    
+    Public Function PesquisanoPDF(PdfFileName As String) As String
+
+
+        Dim sArquivo As String = ""
+
+        'Para tornar a consulta mais rápida, seperam-se os pdf em pastas de anos
+
+        'Pesquisar diários de 2011
+        If Chk2011.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2011\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2012
+        If Chk2012.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2012\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2013
+        If Chk2013.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2013\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2014
+        If Chk2014.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2014\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2015
+        If Chk2015.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2015\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2016
+        If Chk2016.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2016\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2017
+        If Chk2017.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2017\" + PdfFileName
+
+        End If
+
+        'Pesquisar diários de 2017
+        If Chk2018.Checked = True Then
+
+            sArquivo = "\\172.16.0.31\DOL\2018\" + PdfFileName
+
+        End If
+
+        'Abrir o Arquivo PDF
+        Dim oReader As New iTextSharp.text.pdf.PdfReader(sArquivo)
+
+        Dim sOut = ""
+
+        'Procurar dentro do arquivo PDF pelo texto informado
+        For i = 1 To oReader.NumberOfPages
+            Dim its As New iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy
+
+            sOut &= iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(oReader, i, its)
+        Next
+
+        Return sOut
+
+
+
+    End Function
+
+    Protected Sub TxtPesq_TextChanged(sender As Object, e As EventArgs) Handles TxtPesq.TextChanged
+
+        TxtResposta.Text = ""
+
+        Chk2011.Enabled = True
+        Chk2012.Enabled = True
+        Chk2013.Enabled = True
+        Chk2014.Enabled = True
+        Chk2015.Enabled = True
+        Chk2016.Enabled = True
+        Chk2017.Enabled = True
+
+
+    End Sub
+
+    Protected Sub Chk2011_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2011.CheckedChanged
+
+        If Chk2011.Checked = True Then
+
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+
+        End If
+
+    End Sub
+
+    Protected Sub Chk2012_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2012.CheckedChanged
+        If Chk2012.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+
+        End If
+
+
+    End Sub
+
+    Protected Sub Chk2013_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2013.CheckedChanged
+
+        If Chk2013.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+        End If
+
+    End Sub
+
+    Protected Sub Chk2014_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2014.CheckedChanged
+
+
+        If Chk2014.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+        End If
+
+
+    End Sub
+
+    Protected Sub Chk2015_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2015.CheckedChanged
+
+        If Chk2015.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+        End If
+
+
+    End Sub
+
+    Protected Sub Chk2016_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2016.CheckedChanged
+
+        If Chk2016.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2017.Checked = False
+            Chk2018.Checked = False
+        End If
+
+
+    End Sub
+
+    Protected Sub bntPesquisar_Click(sender As Object, e As ImageClickEventArgs) Handles bntPesquisar.Click
+
+        If TxtPesq.Text <> "" Then
+
+
+            'Pesquisar somente os arquivos PDF e pôr em ordem na pasta informada
+            Dim DirDiretorio As DirectoryInfo = New DirectoryInfo(sEndereco)
+            Dim oFileInfoCollection() As FileInfo
+            Dim oFileInfo As FileInfo
+            Dim i As Integer
+
+            TxtResposta.Text = ""
+
+            'Pesquisar diários de 2011
+            If Chk2011.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2011\")
+
+            End If
+
+            'Pesquisar diários de 2012
+            If Chk2012.Checked = True Then
+
+                Chk2011.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2012\")
+
+            End If
+
+            'Pesquisar diários de 2013
+            If Chk2013.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2011.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2013\")
+
+            End If
+
+            'Pesquisar diários de 2014
+            If Chk2014.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2011.Checked = False
+                Chk2013.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2014\")
+
+            End If
+
+            'Pesquisar diários de 2015
+            If Chk2015.Checked = True Then
+
+
+                Chk2011.Checked = False
+                Chk2012.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2015\")
+
+            End If
+
+            'Pesquisar diários de 2016
+            If Chk2016.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2011.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2017.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2016\")
+
+            End If
+
+            'Pesquisar diários de 2017
+            If Chk2017.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2011.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2018.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2017\")
+
+            End If
+
+            'Pesquisar diários de 2018
+            If Chk2018.Checked = True Then
+
+                Chk2012.Checked = False
+                Chk2011.Checked = False
+                Chk2013.Checked = False
+                Chk2014.Checked = False
+                Chk2015.Checked = False
+                Chk2016.Checked = False
+                Chk2017.Checked = False
+
+                DirDiretorio = New DirectoryInfo("\\172.16.0.31\DOL\2018\")
+
+            End If
+
+            'Selecinar os arquivos PDF´s da pasta indicada
+            oFileInfoCollection = DirDiretorio.GetFiles("*.pdf")
+            '### Lista somente os PDFps
+
+
+            'Texto encontrado
+            Stexto = ""
+
+            'Para cada arquivo PDF contido na pasta indicada...
+            For i = 0 To oFileInfoCollection.Length() - 1
+
+                'Selecionar um arquivo
+                oFileInfo = oFileInfoCollection.GetValue(i)
+
+
+                'Abrir cada arquivo PDF e extrair o texto do mesmo
+                Stexto = PesquisanoPDF(oFileInfo.Name)
+
+                'Pesquisar a Palavra
+                Dim index As Integer = Stexto.IndexOf(TxtPesq.Text)
+
+                'Se encontrou a palavra pesquisada dentro do arquivo PDF aberto
+                If index >= 0 Then
+
+                    'Pesquisar o LOCAL EXATO onde encontrou a palavra dentro do arquivo PDF
+                    Try
+
+                        Dim index_2 As Integer = Stexto.IndexOf(".", index + 25)
+                        Dim getTexto As String = Stexto.Substring(index, Stexto.IndexOf(".", index + 75) - (index - 5))
+
+                        'Mostrar na caixa de Texto ao usuário o Nome do arquivo PDF encontrado e uma amostra do texto que bate com o que ele forneceu na pesquisa
+                        TxtResposta.Text = TxtResposta.Text + "Texto presente no arquivo: " + oFileInfo.Name + ". Extrato do texto: " + getTexto + "." + vbCrLf
+
+                    Catch ex As Exception
+
+                        'Data: 14/03/2018 13:25h Ajuste fino para 2016
+                        Dim sJuste As Integer = index - 6581
+                        Dim sAjuste2 As Integer = index - 3081
+                        Dim index_2 As Integer = Stexto.IndexOf(".", index - 75)
+                        Dim getTexto As String = Stexto.Substring(sJuste, Stexto.IndexOf(":", sAjuste2))
+                        'Mostrar na caixa de Texto ao usuário o Nome do arquivo PDF encontrado e uma amostra do texto que bate com o que ele forneceu na pesquisa
+                        TxtResposta.Text = TxtResposta.Text + "Texto presente no arquivo: " + oFileInfo.Name + ". Extrato do texto: " + getTexto + "." + vbCrLf
+
+                    End Try
+
+
+
+
+
+                End If
+
+            Next
+
+            TxtResposta.Enabled = True
+
+
+            TxtResposta.Focus()
+
+        Else
+
+            TxtResposta.Text = "TEXTO DE PESQUISA EM BRANCO! DIGITE UM TEXTO!!"
+
+        End If
+
+    End Sub
+
+    Protected Sub Chk2017_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2017.CheckedChanged
+
+        If Chk2017.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2018.Checked = False
+
+        End If
+
+    End Sub
+    '03/0/2018 08:41h
+    'Criando pesquisa de PDF´s de 2018
+    'Desenvolvedor: Leonardo Metelys
+    Protected Sub Chk2018_CheckedChanged(sender As Object, e As EventArgs) Handles Chk2018.CheckedChanged
+
+        If Chk2018.Checked = True Then
+
+            Chk2011.Checked = False
+            Chk2012.Checked = False
+            Chk2013.Checked = False
+            Chk2014.Checked = False
+            Chk2015.Checked = False
+            Chk2016.Checked = False
+            Chk2017.Checked = False
+
+        End If
+
+
+    End Sub
+End Class
---
+++ BuscarPDF/BuscarPDF/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,99 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="BuscarPDF._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1> 
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/Images/brasaomini.png" />
+&nbsp;PESQUISA DE TEXTOS NO DIÁRIO OFICIAL DA ALEAM. v6.2018</h1>
+                <h4 <span style="color:#27408B;margin-left:10px;">Selecione UM ANO. Informe o texto. A lista de diários oficiais que contêm a frase de pesquisa será preenchida abaixo. A pesquisa pode consumir certo tempo... </span></h4> 
+                <h4 <span style="color:#27408B;margin-left:10px;">Os diários Oficiais tem como nomeclatura a Edição e a data. Exemplo: Edicao04810102011.pdf (Edição 048, de 10/10/2011). Após fazer sua pesquisa para ter acesso ao PDF vá até o Portal ALEAM (www.ale.am.gov.br) e, no menu           
+                TRANSPARÊNCIA, selecione a opção DIÁRIO OFICIAL. Informe o ANO e o MÊS e clique em consultar para ter acesso a Edição desejada.</span></h4>
+            </hgroup>
+          
+        </div>
+    </section>
+    <br/>
+</asp:Content>
+
+
+ 
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+     <br/> 
+      <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/> 
+    <div style="text-align:left; margin-left:11px; ">
+
+    <style type="text/css">
+    label { display: inline-block; }
+    </style>      
+    <asp:CheckBox ID="Chk2011"  Text="2011" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2012" style="text-align:left; margin-left:20px;" Text="2012" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2013" style="text-align:left; margin-left:20px;"  Text="2013" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2014" style="text-align:left; margin-left:20px;"  Text="2014" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2015" style="text-align:left; margin-left:20px;"  Text="2015" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2016" style="text-align:left; margin-left:20px;"  Text="2016" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2017" style="text-align:left; margin-left:20px;"  Text="2017" runat="server" AutoPostBack="True" />
+    <asp:CheckBox ID="Chk2018" style="text-align:left; margin-left:20px;"  Text="2018" runat="server" AutoPostBack="True" />
+    
+   </div>      
+     <br />      
+   <div style="text-align:left; margin-left:15px; ">
+    <asp:Label ID="Label1" runat="server" Text="Defina o texto a pesquisar: "></asp:Label>
+   </div>  
+  
+   <div class="blocoGrupoCampos">
+   <div style="text-align:left; margin-left:15px; height: 35px;">
+   <asp:TextBox ID="TxtPesq" runat="server" Width="429px" Height="27px"></asp:TextBox>
+             <asp:ImageButton ID="bntPesquisar" runat="server" 
+                        ImageUrl="~/Images/pesq.jpg" 
+                        Style="background-image: url(~/Styles/Imagens/pesqback.jpg);
+                            background-repeat: no-repeat; margin-top:15px;  background-position: right;" 
+                            ForeColor="Black"  Height="22px" Width="104px" />                  
+                </div>
+       </div>
+    </br>
+    </br>
+ 
+    <asp:TextBox ID="TxtResposta" runat="server" style="margin-left:15px;" Height="123px" TextMode="MultiLine" Width="1000px" Enabled="False"></asp:TextBox>
+
+    <h3> Instruções de uso:</h3>
+    <ol class="round">
+        <li class="one">
+            <h5>Iniciando</h5>
+            Selecione um ano para que a pesquisa possa ter mais rapidez na busca.
+        </li>
+        <li class="two">
+            <h5>Fazer pesquisa</h5>
+            Informe uma palavra concernente ao que procura. Por exemplo: AMAZONPREV ou o nome da pessoa em letras maiúsculas. Clique em Pesquisar.             
+        </li>
+        <li class="three">     
+            <h5>Nova pesquisa</h5>       
+            Para realizar uma nova busca simplesmente clique no campo texto em "Defina o texto a pesquisar". Os anos serão novamente habilitados para escolha e nova consulta.  
+        </li>
+        <li class="three">     
+            <h5>Lista de Diários Oficiais</h5>       
+            Cada novo Diário Oficial é acrescentado no dia seguinte à sua publicação. A lista de diários oficiais presente em nosso banco de dados é sempre revisada.   
+        </li>
+    </ol> 
+
+    <div class="content-wrapper">
+            <div STYLE="font-family: Humnst777 Cn BT; font-size: 12px; color: blue" class="float-left">
+                <p>
+                  <b><%:DateTime.Now.Year%> - Assembléia Legislativa do Estado do Amazonas. Diretoria de Informática. Gerência de Aplicativos. Tel.: 3183-4340. Desenvolvedor: Leonardo Metelys.</b>
+                </p>
+            </div>
+        </div>
+</div>  
+       
+</asp:Content>
+<asp:Content ID="Content1" runat="server" contentplaceholderid="HeadContent">
+   
+    
+    
+</asp:Content>
+
